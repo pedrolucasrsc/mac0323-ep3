af3faaab2120e5db276ef42567c8974a4d066e37
pedrolucasrsc <pedrosicampos08@gmail.com>
2022-06-15 16:46:37 -0300

    Fixed bug in fila

--- palavrasEncostadas.cpp
+++ palavrasEncostadas.cpp
@@ -21,26 +21,50 @@ int main() {
 
    */
 
-  Ts23<std::string,std::vector<std::string>> dict;
-  int V, E = 0, word;
+  TsA23<std::string,std::vector<std::string>> dict;
+  int V, E = 0;
 
   std::cin >> V;
   for (int v = 0; v < V; v++) {
+	std::string word;
     std::cin >> word;
     for (int i = 0; i < word.length(); i++) {
       std::string bucket;
       bucket = word.substr(0,i) + "_" + word.substr(i+1,word.length()-i-1);
-      if (dict.isIn(bucket))
-	dict.value(bucket).push_back(word);
+      if (dict.isIn(bucket)) {
+		std::vector<std::string> wv;
+		wv = dict.value(bucket);
+		wv.push_back(word);
+  		dict.add(bucket,wv);
+	  }
       else {
-	std::vector<std::string> wv;
-	wv.push_back(word);
-	dict.add(bucket,wv);
+    	std::vector<std::string> wv;
+    	wv.push_back(word);
+	    dict.add(bucket,wv);
       }
     }
   }
 
-  Fila<std::string> vAdj;
-  for 
+  Fila<std::string> fila;
+  for (int i = 0; i < dict.size(); i++) {
+	std::string bucket = dict.select(i);
+	std::vector<std::string> wv;
+	wv = dict.value(bucket);
+	for (int iw = 0; iw < wv.size(); iw++) {
+	  for (int jw = iw+1; jw < wv.size(); jw++) {
+		std::string u = wv[iw], v = wv[jw];
+		//	std::cout << u << " " << v << "\n";
+		fila.enqueue(u);
+		fila.enqueue(v);
+		E++;
+	  }
+	}
+  }
+
+  std::cout << V << " " << E << "\n";
+  while (!fila.isEmpty()) {
+	std::string u = fila.dequeue(), v = fila.dequeue();
+   	std::cout << u << " " << v << "\n";
+  }
   
 }
