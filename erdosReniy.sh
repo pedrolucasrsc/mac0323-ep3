21a19edd91ccf50254e5b486ad12d723c761b0b5
pedrolucasrsc <pedrosicampos08@gmail.com>
2022-06-17 17:18:22 -0300

    added redeSocial and salaDeAula

--- erdosReniy.sh
+++ erdosReniy.sh
@@ -5,3 +5,6 @@ for V in {256,512,1024,2048,4096,8192,16384}; do
 	./arestasAleatorias.out $V $(bc -l <<< "(1-0.1)/$V") > ./grafos/$(echo $V)"minusE.txt"
 	./arestasAleatorias.out $V $(bc -l <<< "(1+0.1)/$V") > ./grafos/$(echo $V)"plusE.txt"	
 done
+./arestasAleatorias.out 100000 $(bc -l <<< "50/100000") > ./grafos/redeSocial.i
+./arestasAleatorias.out 60 $(bc -l <<< "3/60") > ./grafos/salaDeAula.i
+
