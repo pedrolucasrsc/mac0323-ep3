6f99849024bc3b3fdfe7fa66841feb2195b1c3db
pedrolucasrsc <pedrosicampos08@gmail.com>
2022-06-18 09:04:33 -0300

    all distants are compared

--- testa6Graus.cpp
+++ testa6Graus.cpp
@@ -15,7 +15,6 @@ int main() {
   Bag<int> *elemComp;
   elemComp = new Bag<int>[V];
   graph.componentes(&ncomp,&tamComp);
-  graph.qualComp(&comp);
   graph.quemComp(&elemComp);
 
   // Encontra índice de maior componente
@@ -25,26 +24,38 @@ int main() {
 	  max = tamComp[i];
 	  imax = i;
 	}
-  // calcula distâncias a partir de vértice que está na maior componente
-  int iu = elemComp[imax].at(0);
-  std::string u = graph.nomes[iu];
-  graph.distancias(u,&dist);
+  // calcula distâncias na maior componente
+  int maxdist = 0, contadist6 = 0, media = 0, n = 0;
+  std::string udistante, vdistante;
+  for (int i = 0; i < tamComp[imax]; i++) {
+    int iu = elemComp[imax].at(i);
+    std::string u = graph.nomes[iu];
+    //    std::cout << u << "\n";
+    graph.distancias(u,&dist);
+    for (int j = iu+1; j < V; j++) {
+      if (maxdist < dist[j]) {
+        maxdist = dist[j];
+        vdistante = graph.nomes[j];
+        udistante = u;
+      }
+      if (0 < dist[j]) {
+        media += dist[j];
+        n++;
+        if (dist[j] <= 6)
+          contadist6++;
+      }
+    }
+  }
+  media /= n;
+  contadist6;
   
   std::cout << "O Grafo possuí:\n";
   std::cout << "\t" << V << " vértices\n";
   std::cout << "\t" << E << " arestas\n";
   std::cout << "\t" << ncomp << " componentes\n";
-  int maxdist = 0, contadist6 = 0;
-  std::string vdist = u;
-  for (int i = 0; i < V; i++) {
-	if (maxdist < dist[i]) {
-	  maxdist = dist[i];
-	  vdist = graph.nomes[i];
-	}
-	if (0 < dist[i] && dist[i] <= 6)
-	  contadist6++;
-  }
-  std::cout << "A componente de " << u << " tem " << tamComp[comp[iu]] << " vértices:\n";
-  std::cout << u << " está conectada a " << contadist6 << " vértices numa distâncias menor que 6\n";
-  std::cout << u << "----" << vdist << " tem a maior distância de " << maxdist << " na componente!!\n";
+  std::cout << "A componente de " << udistante << " tem " << tamComp[imax] << " vértices, sobre a componente:\n";
+  std::cout << "A média das distâncias entre dois vértices é de " << media << "\n";
+  std::cout << "Há " << contadist6 << " conexões a uma distância de 6 graus ou menos\n";
+  std::cout << udistante << "----" << vdistante << " tem a maior distância de " << maxdist << " na componente!!\n";
+  
 }
