1200bc471f7a7f85d60a54fab3b962fe3d9f8456
pedrolucasrsc <pedrosicampos08@gmail.com>
2022-06-18 10:08:47 -0300

    porcentage and otimization

--- testa6Graus.cpp
+++ testa6Graus.cpp
@@ -32,21 +32,23 @@ int main() {
     std::string u = graph.nomes[iu];
     //    std::cout << u << "\n";
     graph.distancias(u,&dist);
-    for (int j = iu+1; j < V; j++) {
-      if (maxdist < dist[j]) {
-        maxdist = dist[j];
-        vdistante = graph.nomes[j];
+    for (int j = iu+1; j < tamComp[imax]; j++) {
+      int iv = elemComp[imax].at(j);
+      if (maxdist < dist[iv]) {
+        maxdist = dist[iv];
+        vdistante = graph.nomes[iv];
         udistante = u;
       }
-      if (0 < dist[j]) {
-        media += dist[j];
+      if (0 < dist[iv]) {
+        media += dist[iv];
         n++;
-        if (dist[j] <= 6)
+        if (dist[iv] <= 6)
           contadist6++;
       }
     }
   }
   media /= n;
+  double porcentagem = (double)contadist6/n;
   contadist6;
   
   std::cout << "O Grafo possuí:\n";
@@ -55,7 +57,7 @@ int main() {
   std::cout << "\t" << ncomp << " componentes\n";
   std::cout << "A componente de " << udistante << " tem " << tamComp[imax] << " vértices, sobre a componente:\n";
   std::cout << "A média das distâncias entre dois vértices é de " << media << "\n";
-  std::cout << "Há " << contadist6 << " conexões a uma distância de 6 graus ou menos\n";
+  std::cout << porcentagem*100 << "% das conexões a uma distância de 6 graus ou menos\n";
   std::cout << udistante << "----" << vdistante << " tem a maior distância de " << maxdist << " na componente!!\n";
   
 }
