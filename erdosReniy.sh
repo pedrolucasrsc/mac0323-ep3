9c6886849cb2520b0dff5e83481281b417d3906f
pedrolucasrsc <pedrosicampos08@gmail.com>
2022-06-18 11:08:50 -0300

    upgraded E for rede

--- erdosReniy.sh
+++ erdosReniy.sh
@@ -7,5 +7,5 @@ for V in {256,512,1024,2048,4096,8192,16384}; do
 done
 ./arestasAleatorias.out 10000 $(bc -l <<< "100/100000") > ./grafos/redeSocial.in
 ./arestasAleatorias.out 3000 $(bc -l <<< "50/3000") > ./grafos/ime.in
-./arestasAleatorias.out 60 $(bc -l <<< "3/60") > ./grafos/salaDeAula.in
+./arestasAleatorias.out 60 $(bc -l <<< "5/60") > ./grafos/salaDeAula.in
 
