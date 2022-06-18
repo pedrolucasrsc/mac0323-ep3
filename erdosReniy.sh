3322ae082889590c121dffd58e0c6d7ac5f0d172
pedrolucasrsc <pedrosicampos08@gmail.com>
2022-06-18 09:04:08 -0300

    added ime.in

--- erdosReniy.sh
+++ erdosReniy.sh
@@ -5,6 +5,7 @@ for V in {256,512,1024,2048,4096,8192,16384}; do
 	./arestasAleatorias.out $V $(bc -l <<< "(1-0.1)/$V") > ./grafos/$(echo $V)"minusE.txt"
 	./arestasAleatorias.out $V $(bc -l <<< "(1+0.1)/$V") > ./grafos/$(echo $V)"plusE.txt"	
 done
-./arestasAleatorias.out 100000 $(bc -l <<< "50/100000") > ./grafos/redeSocial.i
-./arestasAleatorias.out 60 $(bc -l <<< "3/60") > ./grafos/salaDeAula.i
+./arestasAleatorias.out 10000 $(bc -l <<< "100/100000") > ./grafos/redeSocial.in
+./arestasAleatorias.out 3000 $(bc -l <<< "50/100000") > ./grafos/ime.in
+./arestasAleatorias.out 60 $(bc -l <<< "3/60") > ./grafos/salaDeAula.in
 
